65dd17869ac04f5ab3c2e3b3a75e8cd320c6cd82
Amber <adc351@gmail.com>
2018-11-02 20:46:55 -0700

    adding files to checkpoint 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+
+
+var config = {
+	apiKey: "AIzaSyDVWXfO-dxsV_bA7OGXKZDG0gbmJW9jYHs",
+    authDomain: "bloc-messages.firebaseapp.com",
+    databaseURL: "https://bloc-messages.firebaseio.com",
+    projectId: "bloc-messages",
+    storageBucket: "bloc-messages.appspot.com",
+    messagingSenderId: "400169077004"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
