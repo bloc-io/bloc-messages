4fd3cc0497305e797191ed6158af409429065a49
Amber <adc351@gmail.com>
2018-11-06 13:20:31 -0800

    checkpoint 3: done

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,24 +3,36 @@ import React, { Component } from 'react';
 
 class RoomList extends Component {
 	constructor(props) {
-		super(props); 
-		this.state= { 
+		super(props);
+		this.state= {
 			rooms: []
 		};
-		
+
 		this.roomsRef = this.props.firebase.database().ref('rooms');
 		}
-		
+
 		componentDidMount() {
 			this.roomsRef.on('child_added', snapshot => {
        			const room = snapshot.val();
        			room.key = snapshot.key;
-       			this.setState({ rooms: this.state.rooms.concat( room ) })	 
+       			this.setState({ rooms: this.state.rooms.concat( room ) })
 		});
 		}
-	
-		
-		
+
+		createRoom(newRoomName) {
+			if (!this.state.newRoomName){ return }
+			this.roomsRef.push({
+				name: this.state.newRoomName
+			});
+			this.setState({ newRoomName:" " });
+		}
+
+		handleChange(e){
+			e.preventDefault(); 
+			this.setState({ newRoomName: e.target.value })
+		}
+
+
    render() {
    	return (
         <div>
@@ -31,10 +43,15 @@ class RoomList extends Component {
         			)
         		})}
         	</ul>
+					<form onSubmit={ (e) => this.createRoom(e)}>
+						<legend>Create a New Room</legend>
+						<input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e)}/>
+						<input type="submit" value="Submit" />
+					</form>
       	</div>
      );
    }
 }
 
 
-export default RoomList; 
+export default RoomList;
